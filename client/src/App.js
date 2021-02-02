86f5fd0b36886a3e9f469bfeda929f782269d689
tpoiriersio <poiriertheoo@gmail.com>
2021-02-02 15:07:17 +0100

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
 import './App.css';
-import Header from './components/ui/Header'
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
